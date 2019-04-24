fb46cd20acab1c65644c965dfd5dba54971de1b2
Poliard Djimmy <poliarddjimmy@gmail.com>
2019-04-24 18:32:56 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -11,7 +11,7 @@ class EmailParser
   end 
 
   def parse 
-    email.split(/[,?\s]/).collect do |em|
+    email.split(/\w+@\w+\.{1}[a-zA-Z]{2,}/).collect do |em|
       em.split(',')
     end
     .flatten.uniq 
